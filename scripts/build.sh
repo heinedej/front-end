51a251adeabf4d8eb775ccd9326496e160911ea5
Heine de Jager <heinedej@gmail.com>
2019-07-19 10:51:22 +0200

    One more, again

--- scripts/build.sh
+++ scripts/build.sh
@@ -23,7 +23,7 @@ fi
 CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
 echo $CODE_DIR
 
-#REPO=${GROUP}/$(basename front-end);
-REPO=heinedej/$(basename front-end);
+# REPO=${GROUP}/$(basename front-end);
+# REPO=heinedej/$(basename front-end);
 
 $DOCKER_CMD build -t heinedej/front-end:${COMMIT} .
